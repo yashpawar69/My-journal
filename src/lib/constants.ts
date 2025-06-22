6f75f5ab0d9707b74fe42b05f9f5247e7582dad3
Yash pawar <vinaya234bba@gmail.com>
2025-06-22 13:56:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const DEBOUNCE_DELAY = 500; // milliseconds
+export const DEBOUNCE_DELAY = 2000; // milliseconds
 export const APP_NAME = "My Journal";
 export const MAX_POST_CONTENT_PREVIEW_LENGTH = 150;
