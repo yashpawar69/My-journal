af3e9ff116867fa0d7cff3b13bfd7c3d89115ebc
Yash pawar <vinaya234bba@gmail.com>
2025-06-22 06:32:57 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <h1 class="text-2x

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const DEBOUNCE_DELAY = 500; // milliseconds
-export const APP_NAME = "PersistJournal";
+export const APP_NAME = "My Journal";
 export const MAX_POST_CONTENT_PREVIEW_LENGTH = 150;
